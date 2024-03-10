cfbb44988eeb8d02b6393a1faa222ee0a0a3d826
Georgi Georgiev <ge.georgiev95@gmail.com>
2024-03-10 14:17:11 +0200

    Get random 5 cities

--- Task2/main.py
+++ Task2/main.py
@@ -1,8 +1,13 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
+import os.path
 import requests
+import pandas
+import random
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
+
 
 class WeatherApp(QWidget):
+
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Weather App")
@@ -12,21 +17,38 @@ class WeatherApp(QWidget):
         self.setLayout(self.layout)
         self.get_weather_data()
 
+
     def create_city_labels(self):
         for _ in range(5):
             city_label = QLabel()
             self.city_labels.append(city_label)
             self.layout.addWidget(city_label)
 
+
     def get_weather_data(self):
-        cities = ["London", "Paris", "Berlin", "New York", "Tokyo"]
+        cities = random.sample(self.get_all_cities(), 5)
         API_KEY = "f68e81d7220e100810fc7350499502e9"
+
         for i, city in enumerate(cities):
             response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric")
             data = response.json()
             temperature = data["main"]["temp"]
             humidity = data["main"]["humidity"]
-            self.city_labels[i].setText(f"{city}: Temperature - {temperature}°C, Humidity - {humidity}%")
+            self.city_labels[i].setText(f"{city}: Temperature : {temperature}°C, Humidity : {humidity}%")
+    
+
+    def get_all_cities(self)-> None:
+        if not os.path.isfile('cities_list.xlsx'):
+            all_cities_url = 'https://openweathermap.org/storage/app/media/cities_list.xlsx'
+            data = requests.get(all_cities_url)
+        
+            with open('cities_list.xlsx', 'wb') as output:
+                output.write(data.content)
+
+        excel_data_df = pandas.read_excel('cities_list.xlsx')
+
+        return excel_data_df['name'].to_list()
+    
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
